023f6fa04d3f456ec39bad48afa4f05bbf084ece
Peter Macko <petermacko90@gmail.com>
2018-07-24 11:13:28 +0200

    lifting state up

--- src/App.js
+++ src/App.js
@@ -1,41 +1,101 @@
 import React, { Component } from 'react';
 import ProfileForm from './components/ProfileForm/ProfileForm';
 import Games from './components/Games/Games';
+import Notification from './components/Notification/Notification';
+import Spinner from './components/Spinner/Spinner';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       profile: '',
-      ownedGames: {}
+      ownedGames: {},
+      notification: {
+        show: false,
+        text: ''
+      },
+      showSpinner: false
     };
   }
 
-  getOwnedGames = (data) => {
-    this.setState({ownedGames: data});
+  hideNotification = () => {
+    this.setState({notification: {
+      show: false,
+      text: ''
+    }});
   }
 
-  setProfile = (data) => {
-    this.setState({profile: data});
+  onInputChange = (event) => {
+    this.setState({profile: event.target.value});
+  }
+
+  onPressEnter = (e) => {
+    if (e.key === 'Enter' && this.state.profile) {
+      this.onButtonSubmit();
+    }
+  }
+
+  onButtonSubmit = () => {
+    const { profile } = this.state;
+    
+    this.hideNotification();
+
+    if (!profile) {
+      this.setState({notification: {
+        show: true,
+        text: 'Empty SteamID!'
+      }});
+      return;
+    }
+
+    this.setState({showSpinner: true});
+
+    fetch('https://gamesuggest-api.herokuapp.com/getownedgames', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        profile: profile
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (Object.keys(data.response).length === 0) {
+          throw new Error('SteamID does not exist!');
+        }
+        this.setState({ownedGames: data.response});
+        this.setState({showSpinner: false});
+      })
+      .catch(error => {
+        this.setState({notification: {
+          show: true,
+          text: 'SteamID does not exist!'
+        }});
+        this.setState({ ownedGames: {} });
+        this.setState({showSpinner: false});
+      }); 
   }
 
   render() {
     const {games} = this.state.ownedGames;
-    const {profile} = this.state;
+    const {profile, showSpinner} = this.state;
+    const {text, show} = this.state.notification;
 
     return (
       <div className="helvetica">
+        {
+          show &&
+            <Notification text={text} onClick={this.hideNotification} />
+        }
+        { showSpinner && <Spinner /> }
         <header className="bg-navy gold">
           <h1 className="f1-l f2-m f3 tc ma0 pa3">Games Library</h1>
         </header>
         <ProfileForm
-          getOwnedGames={this.getOwnedGames}
-          setProfile={this.setProfile}
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+          onPressEnter={this.onPressEnter}
         />
-        {
-          profile &&
-            <Games games={games} profile={profile} />
-        }
+        { games && <Games games={games} profile={profile} /> }
       </div>
     );
   }
--- src/components/ProfileForm/ProfileForm.js
+++ src/components/ProfileForm/ProfileForm.js
@@ -1,108 +1,29 @@
 import React from 'react';
-import Notification from '../Notification/Notification';
-import Spinner from '../Spinner/Spinner';
 import '../../../node_modules/font-awesome/css/font-awesome.css';
 
-class ProfileForm extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			profile: '',
-			notification: {
-				show: false,
-				text: ''
-			},
-			showSpinner: false
-		}
-	}
+const ProfileForm = ({ onInputChange, onButtonSubmit, onPressEnter }) => {
 
-	onProfileChange = (event) => {
-    this.setState({profile: event.target.value});
-  }
-
-  handleKeyPress = (e) => {
-  	if (e.key === 'Enter' && this.state.profile) {
-  		this.onSubmit();
-  	}
-  }
-
-  hideNotification = () => {
-  	this.setState({notification: {
-      show: false,
-      text: ''
-    }});
-  }
-
-  onSubmit = () => {
-  	const { profile } = this.state;
-  	
-  	this.hideNotification();
-
-  	if (!profile) {
-			this.setState({notification: {
-        show: true,
-        text: 'Empty SteamID!'
-      }});
-  		return;
-  	}
-
-  	this.setState({showSpinner: true});
-
-  	fetch('https://gamesuggest-api.herokuapp.com/getownedgames', {
-  		method: 'post',
-  		headers: {'Content-Type': 'application/json'},
-  		body: JSON.stringify({
-  			profile: profile
-  		})
-  	})
-  		.then(response => response.json())
-  		.then(data => {
-  			if (Object.keys(data.response).length === 0) {
-  				throw new Error('SteamID does not exist!');
-  			}
-  			this.props.getOwnedGames(data.response);
-  			this.props.setProfile(profile);
-  			this.setState({showSpinner: false});
-  		})
-  		.catch(error => {
-  			this.setState({notification: {
-	        show: true,
-	        text: 'SteamID does not exist!'
-	      }});
-	      this.setState({showSpinner: false});
-  		});	
-  }
-
-	render() {
-		const {text, show} = this.state.notification;
-		const {showSpinner} = this.state;
-
-		return (
-			<div className="mw6 center mt4">
-				{
-					show &&
-						<Notification text={text} onClick={this.hideNotification} />
-				}
-				{ showSpinner && <Spinner /> }
-				<div className="form">
-					<input
-						className="w-60-ns w-100 pa1 ba b--yellow yellow bg-dark-blue"
-						type="text"
-						placeholder="SteamID"
-						name="profile"
-						onChange={this.onProfileChange}
-						onKeyPress={this.handleKeyPress}
-						autoFocus="autofocus"
-					/>
-					<button
-						onClick={this.onSubmit}
-						className="games w-40-ns w-100 ba b--yellow yellow bg-dark-blue hover-dark-blue bg-animate hover-bg-yellow pa1 pointer">
-						<i className="fa fa-steam-square"></i> Get games
-					</button>
-				</div>
+	return (
+		<div className="mw6 center mt4">
+			<div className="form">
+				<input
+					className="w-60-ns w-100 pa1 ba b--yellow yellow bg-dark-blue"
+					type="text"
+					placeholder="SteamID"
+					name="profile"
+					onChange={onInputChange}
+					onKeyPress={onPressEnter}
+					autoFocus="autofocus"
+				/>
+				<button
+					onClick={onButtonSubmit}
+					className="games w-40-ns w-100 ba b--yellow yellow bg-dark-blue hover-dark-blue bg-animate hover-bg-yellow pa1 pointer">
+					<i className="fa fa-steam-square"></i> Get games
+				</button>
 			</div>
-		);
-	}
+		</div>
+	);
+	
 }
 
 export default ProfileForm;

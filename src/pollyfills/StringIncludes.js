9422d83be646d31aea668a5a691a218d48f6ba88
Peter Macko <petermacko90@gmail.com>
2018-07-30 20:53:17 +0200

    added pollyfill for String.includes

---
+++ src/pollyfills/StringIncludes.js
@@ -0,0 +1,14 @@
+/* eslint no-extend-native: "off" */
+if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+    
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
